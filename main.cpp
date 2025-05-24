c808c6ce51ef984ba201d236ee9f276199c78fbb
suhasg1 <suhas.g@s.amity.edu>
2025-05-24 12:41:24 +0530

    simple bank service structure

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// main.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "----------------------------------" << endl;
+    cout << "   Welcome to ATM Service" << endl;
+    cout << "----------------------------------" << endl;
+
+    cout << "\n--- ATM Services ---" << endl;
+    cout << "1. Check Balance" << endl;
+    cout << "2. Deposit Cash" << endl;
+    cout << "3. Withdraw Cash" << endl;
+    cout << "4. PIN Change" << endl;
+    cout << "5. Exit" << endl;
+
+    return 0;
+}
+
